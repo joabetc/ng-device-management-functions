106a1edabc8949d4885f1e5c30ce57ac6fddf082
Joabe Tavares Costa <jtcosta@Joabe.local>
2019-04-09 17:21:17 -0300

    Creating a HTTP endpoint #2

--- functions/index.js
+++ functions/index.js
@@ -9,3 +9,9 @@ exports.addEmployee = functions.database.ref('/user/{userId}')
         const user = snapshot.val();
         return admin.database().ref('/employee').child(user.workerid).set({ 'email': user.email });
     });
+
+exports.employeeEmail = functions.https.onRequest((req, res) => {
+    const workerid = req.query.id;
+    const email = admin.database().ref(`/employee/${workerid}/email`).once('value').then((snapshot) => snapshot.val());
+    res.send(email);
+})
