814dc748dfcd2a64d5c53f12613cf5552eb4a27a
Joabe Costa <joabetc@hotmail.com>
2019-05-31 22:24:12 -0300

    Creating HTTP  for adding Admin role #5

--- functions/index.js
+++ functions/index.js
@@ -15,4 +15,18 @@ exports.employeeEmail = functions.https.onRequest((req, res) => {
     console.log(workerid);
     return admin.database().ref('/employee/'+ workerid).once('value')
         .then((snapshot) => res.send(snapshot.val().email));
-})
+});
+
+exports.addAdminRole = functions.https.onCall((data, context) => {
+    if (context.auth.token.isAdmin !== true) {
+        return {
+            error: "Request not authorized. User must be an administrator to fullfill request."
+        }
+    };
+    const email = data.email;
+    return grantRoleByEmail(email, { isAdmin: true }).then(() => {
+        return {
+            result: "Request fullfilled! " + email + " is noew an administrator;"
+        };
+    });
+});
